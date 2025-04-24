0ce97bbf0ac232a7828dd8d234d4016db1724e3e
Kameleo Team <hello@kameleo.io>
2025-04-24 09:26:35 +0200

    chore: update for 4.0.0

---
+++ src/Kameleo.LocalApiClient/Customization/CookieRequest.cs
@@ -0,0 +1,20 @@
+﻿// ReSharper disable once CheckNamespace
+namespace Kameleo.LocalApiClient.Model
+{
+    public partial class CookieRequest
+    {
+        public CookieRequest(BrowserCookie cookie)
+            : this(
+                cookie.Domain,
+                cookie.Name,
+                cookie.Path,
+                cookie.Value,
+                cookie.HostOnly,
+                cookie.HttpOnly,
+                cookie.Secure,
+                cookie.SameSite,
+                cookie.ExpirationDate
+            )
+        { }
+    }
+}
---
+++ src/Kameleo.LocalApiClient/IKameleoLocalApiClient.cs
@@ -0,0 +1,46 @@
+﻿using Kameleo.LocalApiClient.Api;
+using Kameleo.LocalApiClient.Client;
+
+namespace Kameleo.LocalApiClient
+{
+    /// <summary>
+    /// Encapsualte access to all the provided APIs by Kameleo.CLI.
+    /// </summary>
+    public interface IKameleoLocalApiClient
+    {
+        /// <summary>
+        /// Configuration of the API client.
+        /// </summary>
+        IReadableConfiguration Configuration { get; }
+
+        /// <summary>
+        /// Represents a collection of functions to interact with the Fingerprint API endpoints.
+        /// </summary>
+        IFingerprintApiAsync Fingerprint { get; }
+
+        /// <summary>
+        /// Represents a collection of functions to interact with the Cookie API endpoints.
+        /// </summary>
+        ICookieApiAsync Cookie { get; }
+
+        /// <summary>
+        /// Represents a collection of functions to interact with the Folder API endpoints.
+        /// </summary>
+        IFolderApiAsync Folder { get; }
+
+        /// <summary>
+        /// Represents a collection of functions to interact with the General API endpoints.
+        /// </summary>
+        IGeneralApiAsync General { get; }
+
+        /// <summary>
+        /// Represents a collection of functions to interact with the Profile API endpoints.
+        /// </summary>
+        IProfileApi Profile { get; }
+
+        /// <summary>
+        /// Represents a collection of functions to interact with the Kernel API endpoints.
+        /// </summary>
+        IKernelApi Kernel { get; }
+    }
+}
